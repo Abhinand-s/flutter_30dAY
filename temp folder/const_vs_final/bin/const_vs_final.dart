efd97d99816461beaac696395e3d9c502ed3bbe5
abhinands <abhinands488@gmail.com>
2024-02-22 22:20:46 +0530

    update

---
+++ temp folder/const_vs_final/bin/const_vs_final.dart
@@ -0,0 +1,16 @@
+void main(){
+  final String name ; //used ones never used again
+
+  name='sort name';
+
+  final List<int> numberlist;
+  numberlist=List.empty(); // in case of list we can use final .we can assign n numbers but ones assign we can't replace 
+  numberlist.add(1);
+  numberlist.add(100);
+
+  const String name1 ='hello';//const should declare during intial state.
+ 
+
+
+
+}
